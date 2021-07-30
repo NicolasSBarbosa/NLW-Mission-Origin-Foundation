cfbf89bf1cdfa89558bf9f5bb64db014819a9ecb
Nicolas <nicolasestudos@gmail.com>
2021-07-30 19:50:02 -0300

    UPDATE - rolagem suave

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})

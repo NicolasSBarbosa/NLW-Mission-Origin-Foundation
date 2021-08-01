43cbcb47559841f73653a858accfbe7649f520a7
Nicolas <nicolasestudos@gmail.com>
2021-08-01 20:14:09 -0300

    UPDATE - depoimentos, efeitos ao rolar a pagina e whasapp contato

--- main.js
+++ main.js
@@ -27,3 +27,26 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scroll')
     }
 })
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+    slidePerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials #contact .text, #contact .links
+    `, { interval: 100 }
+)
